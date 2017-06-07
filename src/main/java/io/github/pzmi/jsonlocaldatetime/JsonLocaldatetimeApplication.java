2ee2c57b65594d3a50a5a6e39221a7ab48d871d1
pzmi <p.zmigrodzki@gmail.com>
2017-06-07 18:28:48 +0200

    Initial commit

---
+++ src/main/java/io/github/pzmi/jsonlocaldatetime/JsonLocaldatetimeApplication.java
@@ -0,0 +1,12 @@
+package io.github.pzmi.jsonlocaldatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonLocaldatetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonLocaldatetimeApplication.class, args);
+	}
+}
