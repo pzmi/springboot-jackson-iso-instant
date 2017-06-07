e7d0041368ea902e00db8770b1aa7564bd35d2d5
pzmi <p.zmigrodzki@gmail.com>
2017-06-07 18:28:58 +0200

    Local configuration

--- src/main/java/io/github/pzmi/jsonlocaldatetime/SomethingWithDate.java
+++ src/main/java/io/github/pzmi/jsonlocaldatetime/SomethingWithDate.java
@@ -1,22 +1,25 @@
 package io.github.pzmi.jsonlocaldatetime;
 
-import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
 
 public class SomethingWithDate {
-    private Instant dateTime;
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private LocalDateTime dateTime;
 
     public SomethingWithDate() {
     }
 
-    public SomethingWithDate(Instant dateTime) {
+    public SomethingWithDate(LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 
-    public Instant getDateTime() {
+    public LocalDateTime getDateTime() {
         return dateTime;
     }
 
-    public void setDateTime(Instant dateTime) {
+    public void setDateTime(LocalDateTime dateTime) {
         this.dateTime = dateTime;
     }
 }
